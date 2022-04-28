80ee8d42412c11f032bc27ba32593438604a3014
m-samara <masamara@aspire-services.net>
2022-04-28 16:16:13 -0400

    edeted minor styling for mobile and  donedesktop view

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -5,23 +5,21 @@ class Home extends React.Component {
   render() {
     return (
       <div className={classes.home}>
-          <div className={classes.logo}>
-              OBASE APPAREL
-          </div>
-        <div className={classes.main_img}>
-        </div>
+        <div className={classes.logo}>OBASE APPAREL</div>
+        <div className={classes.main_img}></div>
         <div className={classes.focused + " " + classes.m_50_10}>
           WE'RE COMING SOON
         </div>
         <div className={classes.description + " " + classes.m_50_10}>
-            Hello fellow shoppers! We're currently building our new fashion store.
-             Add your email below to stay up-to-date with announcements and our launch deals.
+          Hello fellow shoppers! We're currently building our new fashion store.
+          Add your email below to stay up-to-date with announcements and our
+          launch deals.
         </div>
-        <div className={classes.m_50_10 + " " + classes.clear_last_m}>
-        <div className={classes.search_container}>
+        <div className={classes.m_50_10}>
+          <div className={classes.search_container}>
             <input placeholder="Email Address" />
             <button>&gt;</button>
-        </div>
+          </div>
         </div>
       </div>
     );
